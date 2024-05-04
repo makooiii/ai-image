48aa5452667c91621aae922804b48f77d3126855
Mark Sapiandante <msapiandante.ccheetah@gmail.com>
2024-05-04 23:26:13 +0800

    update

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -7,7 +7,6 @@ interface MongooseConnection {
     promise: Promise<Mongoose> | null;
 }
 
-// caching
 let cached: MongooseConnection = (global as any).mongoose;
 
 if (!cached) {
@@ -19,11 +18,17 @@ if (!cached) {
 
 export const connectToDatabase = async () => {
     if (cached.conn) return cached.conn;
+
     if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
 
     cached.promise =
         cached.promise ||
-        mongoose.connect(MONGODB_URL, { dbName: "Imaginify", bufferCommands: false });
+        mongoose.connect(MONGODB_URL, {
+            dbName: "imaginify",
+            bufferCommands: false
+        });
+
     cached.conn = await cached.promise;
+
     return cached.conn;
 };
