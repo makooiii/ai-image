ca1d2742676c125d5356a5d2e7a19f793cecd71d
Mark Sapiandante <msapiandante.ccheetah@gmail.com>
2024-05-04 22:42:35 +0800

    update database

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -23,7 +23,7 @@ export const connectToDatabase = async () => {
 
     cached.promise =
         cached.promise ||
-        mongoose.connect(MONGODB_URL, { dbName: "imaginify", bufferCommands: false });
+        mongoose.connect(MONGODB_URL, { dbName: "Imaginify AI", bufferCommands: false });
     cached.conn = await cached.promise;
     return cached.conn;
 };
