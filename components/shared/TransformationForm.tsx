8d17da9db7f513541168e1e10892a1903a254417
Mark Sapiandante <msapiandante.ccheetah@gmail.com>
2024-05-08 17:01:16 +0800

    add function to transformation form

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -28,6 +28,9 @@ import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { updateCredits } from "@/lib/actions/user.actions";
 import MediaUploader from "./MediaUploader";
 import TransformedImage from "./TransformedImage";
+import { getCldImageUrl } from "next-cloudinary";
+import { addImage, updateImage } from "@/lib/actions/image.actions";
+import { useRouter } from "next/navigation";
 
 export const formSchema = z.object({
     title: z.string(),
@@ -52,6 +55,7 @@ const TransformationForm = ({
     const [isTransforming, setIsTransforming] = useState(false);
     const [transformationConfig, setTransformationConfig] = useState(config);
     const [isPending, startTransition] = useTransition();
+    const router = useRouter();
     const intialValues =
         data && action === "Update"
             ? {
@@ -70,8 +74,69 @@ const TransformationForm = ({
     });
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values);
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+        setIsSubmitting(true);
+
+        if (data || image) {
+            const transformationUrl = getCldImageUrl({
+                width: image?.width,
+                height: image?.height,
+                src: image?.publicId,
+                ...transformationConfig
+            });
+
+            const imageData = {
+                title: values.title,
+                publicId: image?.publicId,
+                transformationType: type,
+                width: image?.width,
+                height: image?.height,
+                config: transformationConfig,
+                secureURL: image?.secureUrl,
+                transformationURL: transformationUrl,
+                aspectRatio: values.aspectRatio,
+                prompt: values.prompt,
+                color: values.color
+            };
+
+            if (action === "Add") {
+                try {
+                    const newImage = await addImage({
+                        image: imageData,
+                        userId,
+                        path: "/"
+                    });
+
+                    if (newImage) {
+                        form.reset();
+                        setImage(data);
+                        router.push(`/transformations/${newImage._id}`);
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+
+            if (action === "Update") {
+                try {
+                    const updatedImage = await updateImage({
+                        image: {
+                            ...imageData,
+                            _id: data._id
+                        },
+                        userId,
+                        path: `/tansformations/${data._id}`
+                    });
+
+                    if (updatedImage) {
+                        router.push(`/transformations/${updatedImage._id}`);
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }
+        setIsSubmitting(false);
     }
 
     const onSelectFieldHandler = (value: string, onChangeField: (value: string) => void) => {
@@ -139,13 +204,14 @@ const TransformationForm = ({
                         render={({ field }) => (
                             <Select
                                 onValueChange={value => onSelectFieldHandler(value, field.onChange)}
+                                value={field.value}
                             >
-                                <SelectTrigger className="w-[180px]">
-                                    <SelectValue placeholder="Select Size" />
+                                <SelectTrigger className="select-field">
+                                    <SelectValue placeholder="Select size" />
                                 </SelectTrigger>
                                 <SelectContent>
                                     {Object.keys(aspectRatioOptions).map(key => (
-                                        <SelectItem key={key} value="key" className="select-item">
+                                        <SelectItem key={key} value={key} className="select-item">
                                             {aspectRatioOptions[key as AspectRatioKey].label}
                                         </SelectItem>
                                     ))}
